054f3b10d04fea8c10bff92f6aa0619dca2889bd
touch <tttouch1122@gmail.com>
2023-01-10 15:48:35 +0700

    13.4

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
+
+template <typename T>
+void mySwap(T &,T &); 
 
 int main(){
 	int x, y;
@@ -29,4 +33,10 @@ int main(){
 
 	return 0;
 }
+template <typename T>
+void mySwap(T &x,T &y){
+	T z =x;
+	x = y;
+	y = z;
 
+}

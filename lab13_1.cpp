5fbcc670f29e20e6581a66df300a1db700f6f4ac
touch <tttouch1122@gmail.com>
2023-01-10 15:08:00 +0700

    13.1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+int fibonacci(int x){
+    if(x<=1){
+        return x;
+    }else{
+        return x = fibonacci(x-1) + fibonacci(x-2);
+    }
 }

058ead97e6d2f229f783025b678b940de496c2bc
touch <tttouch1122@gmail.com>
2023-01-10 15:20:48 +0700

    13.1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
-int fibonacci(int x);
+long long int fibonacci(long long int x);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
-int fibonacci(int x){
+long long int fibonacci(long long int x){
     if(x<=1){
         return x;
     }else{
         return x = fibonacci(x-1) + fibonacci(x-2);
     }
+
 }
